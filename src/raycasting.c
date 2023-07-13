41f8d058a7e00bd3c8bd82019348b0551e082ab4
Emma Muller <emuller@k0r1p3.42mulhouse.fr>
2023-07-13 18:56:04 +0200

    yoyoyoyo

--- src/raycasting.c
+++ src/raycasting.c
@@ -31,70 +31,10 @@ int coll_ray(t_game *game, int i, int j)
     return (0);
 }
 
-double	calculate_rayon_horizontal(t_game *game, int i)
-{
-	float	h;
-	double	distance_wall = 100000000;
-	
-	// on  initialise
-	game->ray[0][i].angle = game->player.angle - (N_RAY/2 * DR - (i * DR));
-	game->ray[0][i].angle = adjust_angle(game->ray[0][i].angle);
-	
-	game->ray[0][i].r.x = game->player.coord.x;
-	game->ray[0][i].r.y = game->player.coord.y;
-	
-	//On calcul l'offset
-	game->ray[0][i].o.y = MINIMAP_BLOC_SIZE;
-	game->ray[0][i].o.x = MINIMAP_BLOC_SIZE / tanf(game->ray[0][i].angle);
-
-	//on trouve les coord de la premiere intersection
-	double artan; 
-	artan = -1/tanf(game->ray[0][i].angle);
-	if (game->ray[0][i].angle < PI) // on regarde vers le NORD 
-	{
-		game->ray[0][i].r.y = ((int)(game->player.coord.y / MINIMAP_BLOC_SIZE) * MINIMAP_BLOC_SIZE) - 0.0001;
-		h = (game->player.coord.y - game->ray[0][i].r.y);
-		game->ray[0][i].r.x = game->player.coord.x + (h * artan);
-	}
-	else if (game->ray[0][i].angle > PI) // on regarde vers le SUD
-	{
-		game->ray[0][i].r.y = ((int)(game->player.coord.y / MINIMAP_BLOC_SIZE) * MINIMAP_BLOC_SIZE) + MINIMAP_BLOC_SIZE;
-		h = (game->ray[0][i].r.y - game->player.coord.y);
-		game->ray[0][i].r.x = game->player.coord.x - (h * artan);
-	}
-
-	//on check si c'est un mur en ajoutant l'offset tant que ce n'est pas un mur
-	while (1)
-	{
-		if (coll_ray(game, 0, i))
-		{
-			distance_wall = sqrt(((game->ray[0][i].r.x - game->player.coord.x) * (game->ray[0][i].r.x - game->player.coord.x)) 
-					+ (game->ray[0][i].r.y - game->player.coord.y) * (game->ray[0][i].r.y - game->player.coord.y));
-			break;
-		}
-		else
-		{
-			if (game->ray[0][i].r.x < 0 || game->ray[0][i].r.x >= DISPLAY_WIDTH || game->ray[0][i].r.y < 0 || game->ray[0][i].r.y >= DISPLAY_HEIGHT)
-				break;
-			if (game->ray[0][i].angle < PI)
-			{
-				game->ray[0][i].r.x -= game->ray[0][i].o.x;
-				game->ray[0][i].r.y -= game->ray[0][i].o.y;
-			}
-			else if (game->ray[0][i].angle > PI)
-			{
-				game->ray[0][i].r.x += game->ray[0][i].o.x;
-				game->ray[0][i].r.y += game->ray[0][i].o.y;
-			}
-		}
-	}
-	return (distance_wall);
-}
-
-void	add_offset_vert(t_game *game, int i)
+int	add_offset_vert(t_game *game, int i)
 {
 	if (game->ray[1][i].r.x < 0 || game->ray[1][i].r.x >= DISPLAY_WIDTH || game->ray[1][i].r.y < 0 || game->ray[1][i].r.y >= DISPLAY_HEIGHT)
-		break;
+		return (1);
 	if (game->ray[1][i].angle > (PI / 2) && game->ray[1][i].angle < (3 * PI / 2))
 	{
 		game->ray[1][i].r.x += game->ray[1][i].o.x;
@@ -105,26 +45,80 @@ void	add_offset_vert(t_game *game, int i)
 		game->ray[1][i].r.x -= game->ray[1][i].o.x;
 		game->ray[1][i].r.y -= game->ray[1][i].o.y;	
 	}
+	return (0);
 }
 
-float	check_vert_walls(t_game *game, int i)
+int		add_offset_hori(t_game *game, int i)
+{
+	if (game->ray[0][i].r.x < 0 || game->ray[0][i].r.x >= DISPLAY_WIDTH || game->ray[0][i].r.y < 0 || game->ray[0][i].r.y >= DISPLAY_HEIGHT)
+		return (1);
+	if (game->ray[0][i].angle < PI)
+	{
+		game->ray[0][i].r.x -= game->ray[0][i].o.x;
+		game->ray[0][i].r.y -= game->ray[0][i].o.y;
+	}
+	else if (game->ray[0][i].angle > PI)
+	{
+		game->ray[0][i].r.x += game->ray[0][i].o.x;
+		game->ray[0][i].r.y += game->ray[0][i].o.y;
+	}
+	return (0);
+}
+
+float	check_walls(t_game *game, int i, int side)
 {
 	double	distance_wall;
 
 	distance_wall = 100000000;
 	while (1)
 	{
-		if (coll_ray(game, 1, i))
+		if (coll_ray(game, side, i))
 		{
-			distance_wall = sqrt(((game->ray[1][i].r.x - game->player.coord.x) * (game->ray[1][i].r.x - game->player.coord.x)) 
-					+ (game->ray[1][i].r.y - game->player.coord.y) * (game->ray[1][i].r.y - game->player.coord.y));
+			distance_wall = sqrt(((game->ray[side][i].r.x - game->player.coord.x) * (game->ray[side][i].r.x - game->player.coord.x)) 
+					+ (game->ray[side][i].r.y - game->player.coord.y) * (game->ray[side][i].r.y - game->player.coord.y));
 			break;
 		}
 		else
 		{
-			add_offset_vert(game, i);
+			if (side == 1)
+				if (add_offset_vert(game, i) == 1)
+					break;
+			if (side == 0)
+				if (add_offset_hori(game, i) == 1)
+					break;
 		}
 	}
+	return (distance_wall);
+}
+
+
+
+double	calculate_rayon_horizontal(t_game *game, int i)
+{
+	float	h;
+	double	distance_wall = 100000000;
+	double artan; 
+	
+	game->ray[0][i].angle = game->player.angle - (N_RAY/2 * DR - (i * DR));
+	game->ray[0][i].angle = adjust_angle(game->ray[0][i].angle);
+	game->ray[0][i].r.x = game->player.coord.x;
+	game->ray[0][i].r.y = game->player.coord.y;
+	game->ray[0][i].o.y = MINIMAP_BLOC_SIZE;
+	game->ray[0][i].o.x = MINIMAP_BLOC_SIZE / tanf(game->ray[0][i].angle);
+	artan = -1/tanf(game->ray[0][i].angle);
+	if (game->ray[0][i].angle < PI) 
+	{
+		game->ray[0][i].r.y = ((int)(game->player.coord.y / MINIMAP_BLOC_SIZE) * MINIMAP_BLOC_SIZE) - 0.0001;
+		h = (game->player.coord.y - game->ray[0][i].r.y);
+		game->ray[0][i].r.x = game->player.coord.x + (h * artan);
+	}
+	if (game->ray[0][i].angle > PI) 
+	{
+		game->ray[0][i].r.y = ((int)(game->player.coord.y / MINIMAP_BLOC_SIZE) * MINIMAP_BLOC_SIZE) + MINIMAP_BLOC_SIZE;
+		h = (game->ray[0][i].r.y - game->player.coord.y);
+		game->ray[0][i].r.x = game->player.coord.x - (h * artan);
+	}
+	distance_wall = check_walls(game, i, 0);
 	return (distance_wall);
 }
 
@@ -139,18 +133,18 @@ double	calculate_rayon_vertical(t_game *game, int i)
 	game->ray[1][i].r.y = game->player.coord.y;
 	game->ray[1][i].o.x = MINIMAP_BLOC_SIZE;
 	game->ray[1][i].o.y = MINIMAP_BLOC_SIZE * tanf(game->ray[1][i].angle);
-	if (game->ray[1][i].angle < (PI / 2) || game->ray[1][i].angle > (3 * PI / 2)) // on regarde vers le EST
+	if (game->ray[1][i].angle < (PI / 2) || game->ray[1][i].angle > (3 * PI / 2))
 	{
 		game->ray[1][i].r.x = (int)((int)(game->player.coord.x / MINIMAP_BLOC_SIZE) * MINIMAP_BLOC_SIZE) - 0.0001;
 		h = fabs(game->player.coord.x - game->ray[1][i].r.x);
 		game->ray[1][i].r.y = game->player.coord.y - h * tanf(game->ray[1][i].angle);
 	}
-	if (game->ray[1][i].angle > (PI / 2) && game->ray[1][i].angle < (3 * PI / 2)) // on regarde vers l'OUEST
+	if (game->ray[1][i].angle > (PI / 2) && game->ray[1][i].angle < (3 * PI / 2))
 	{
 		game->ray[1][i].r.x = (int)((int)(game->player.coord.x / MINIMAP_BLOC_SIZE) * MINIMAP_BLOC_SIZE) + MINIMAP_BLOC_SIZE;
 		h = fabs(game->player.coord.x - game->ray[1][i].r.x);
 		game->ray[1][i].r.y = game->player.coord.y + h * tanf(game->ray[1][i].angle);
 	}
-	distance_wall = check_vert_walls(game, i);
+	distance_wall = check_walls(game, i, 1);
 	return (distance_wall);
 }
